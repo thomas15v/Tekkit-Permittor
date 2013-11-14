c0cbc1ceaf7baddd9a8fc2d6c6a94e437bd0bc62
thomas15v <tvanmellaerts@live.be>
2013-11-14 19:58:17 +0100

    Added option to ban blocks globally in regions.

--- src/thomas15v/Worldguardevents.java
+++ src/thomas15v/Worldguardevents.java
@@ -20,6 +20,7 @@ public class Worldguardevents implements Listener {
 	int[] wrenches = {21257,30140,30183,4062,4370};
 	int[] tools = {30119,30124,5582,5587,20257,20259,27003,27002,19297};
 	int[] alwaysblockedtools = {19263,4363,4364,19261,30208,30215};
+	int[] alwaysblockedblocks= {46,237};
 	int [] Containerblocks = {23,25,54,61,62,64,69,70,71,72,77,96,84,93,94,107,192,901,250,246,188,277,2491,207,900,181,251,3120,3131,227,751,233,2050,183,2002,30208,3893,223};
 	int [] UseBlocks = {255};
 	WorldGuardPlugin worldguard;
@@ -76,6 +77,11 @@ public class Worldguardevents implements Listener {
 		if (inarea && (event.getPlayer().getName().equalsIgnoreCase("[RedPower]") || event.getPlayer().getName().equalsIgnoreCase("[computercraft]") )){
 			event.setCancelled(true);			
 		}		
+		
+		if (inarea && functions.InArray(alwaysblockedblocks, block.getTypeId())){
+			event.getPlayer().sendMessage(ChatColor.DARK_RED + "The placement of this block is blocked globally");
+			event.setCancelled(true);
+		}
 		
 	}	
 	@EventHandler(priority = EventPriority.HIGHEST)
